919666839af3de6c3788a3c1f27d6dd5754b0c2c
Christopher Aguilar <christopher.m.aguilar@gmail.com>
2016-08-21 06:22:03 -0400

    I forgot to add 2em margin to 'About' paragraphs

--- app/pages/About.js
+++ app/pages/About.js
@@ -9,7 +9,7 @@ export default class About extends React.Component {
           <h1><strong>Free Code Camp Polls</strong></h1>
           <h3>About</h3>
         </Col>
-        <Col sm={8} smOffset={2}>
+        <Col sm={8} smOffset={2} style={{marginTop: "2em"}}>
           <p>This is a voting app created for <a href="https://freecodecamp.com">Free Code Camp's</a> first dynamic web project. The goal was to create a web app that allows users to view, create, delete, and vote in polls.</p>
           <p>This voting app is a single-page app that uses <a href="https://nodejs.org" target="_blank">Node</a> and <a href="https://expressjs.com" target="_blank">Express</a> to serve the page from <a href="https://heroku.com" target="_blank">Heroku's</a> servers, <a href="http://mongoosejs.com" target="_blank">Mongoose</a> and <a href="https://mlab.com" target="_blank">mLab</a> to store the data, <a href="http://passportjs.org" target="_blank">Passport.js</a> to allow logging in, <a href="https://github.com/reactjs/react-router" target="_blank">React-Router</a> to allow single-page navigation, <a href="http://www.chartjs.org/" target="_blank">Chart.js</a> to draw the poll charts, and <a href="https://facebook.github.io/react/" target="_blank">React</a> to tie all of these things together and present it to users.</p>
           <p>To vote in a poll, simply click the desired poll and use the dropdown box to select which option you'd like to vote for, and click submit.</p>
